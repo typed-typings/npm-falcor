4475f12c6e68d76b732fc35129794a3649accebb
Blake Embrey <hello@blakeembrey.com>
2016-04-27 12:29:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typed Falcor
+
+The type definition for [`falcor`](https://github.com/Netflix/falcor).
+
+## License
+
+MIT
---
+++ test/index.ts
@@ -0,0 +1,7 @@
+/// <reference path="../bundle/main.d.ts" />
+/// <reference path="../typings/main.d.ts" />
+
+import falcor = require('falcor')
+import assert = require('assert')
+
+assert.deepEqual(typeof falcor.Model, 'function')
